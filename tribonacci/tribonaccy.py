457caf02252cc9fbc28f66a7066c3903bdbfd4f5
marto ndegz <martondegz@gmail.com>
2016-08-01 16:42:28 +0300

    updated

---
+++ tribonacci/tribonaccy.py
@@ -0,0 +1,8 @@
+def addition(n):
+    a, b, c = 0, 0, 1
+    while (b < n):
+        print (c)
+        a, b, c = b, c, a + b + c
+
+
+
---
+++ tribonacci/user.py
@@ -0,0 +1,10 @@
+
+
+from tribonaccy import addition
+
+def series():
+
+    users = int(raw_input("what interger?"))
+    print addition(users)
+
+series()
