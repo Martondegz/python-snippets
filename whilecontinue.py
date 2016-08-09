061a6288d8a1a02085ce49b0c994b1f20b9d0b95
Martondegz <martondegz@gmail.com>
2016-08-09 18:32:52 +0300

    Update whilecontinue.py

--- whilecontinue.py
+++ whilecontinue.py
@@ -1,5 +1,5 @@
 while True:
     name = raw_input('enter name:')
-    if name == 'stop':break
+    if name == 'stop':break #breaks the statement so that the age input follows
     age = raw_input('enter age:')
     print ('Hello', name, '=>', int(age) ** 2)
